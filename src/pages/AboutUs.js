6f5134649bef51e0d60571fff66d85c092d40b99
hakan-art <hakan.gezer74@googlemail.com>
2020-12-25 11:39:28 +0100

    4. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,13 @@
 //Page components
 import AboutSection from '../components/AboutSection'
+import ServicesSection from '../components/ServicesSection'
 
 const AboutUs = () => {
     return (
-        <AboutSection />
+        <>
+            <AboutSection />
+            <ServicesSection />
+        </>
     );
 }
 
